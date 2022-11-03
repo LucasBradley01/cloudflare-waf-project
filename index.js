481f06d9192e3dceddb462d4584974cfc2f22c5d
Debian <bitnami@ip-172-26-10-140.us-west-2.compute.internal>
2022-11-03 21:15:56 +0000

    First draft

---
+++ index.js
@@ -0,0 +1,169 @@
+// Collect necessary libraries
+const express = require('express');
+const path = require('path');
+const fetch = require('node-fetch');
+
+// Initialize express app
+const app = express();
+app.use(express.json());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+// Initialze REST API
+app.get('/', (req, res) => {
+    //console.log(req);
+    res.status(200).sendFile(path.join(__dirname, './index.html'))
+});
+app.get('/animate.js', (req, res) => res.status(200).sendFile(path.join(__dirname, './animate.js')));
+app.get('/style.css', (req, res) => res.status(200).sendFile(path.join(__dirname, './style.css')));
+app.post('/login', async (req, res) => {
+    
+    // Get the data from the server api
+    let rawEventsResult = await fetch("https://api.fpjs.io/events/" + req.body.requestId, {
+        method: 'GET',
+        headers: {
+            "Auth-API-Key": "3xWrXmkeq5aKRKQelbLg"
+        },
+        redirect: 'follow'
+    });
+    let jsonEventsResult = await rawEventsResult.json();
+
+    // Check if the response was successful
+    if (jsonEventsResult.error) {
+        res.status(400).send(JSON.stringify({
+            result: 400,
+            description: jsonEventsResult.error.message,
+            error: jsonEventsResult
+        }));
+        return;
+    }
+
+    // Check if the visitorId they give matches the visitorId returned by the server api
+    if (jsonEventsResult.products.identification.data.visitorId !== req.body.visitorId) {
+        res.status(400).send(JSON.stringify({
+            result: 400,
+            description: "visitorId and requestId do not match"
+        }));
+        return;
+    }
+
+    // Check if the requestor is a bot
+    if (!req.body.bot) { // jsonEventsResult.products.botd.data.bot.result !== "bad"
+        res.status(200);
+        res.send(JSON.stringify({
+            result: 200,
+            description: 'Ok'
+        }));
+        return;
+    }
+
+    // Create a filter to be used for blocking this ip
+    let rawCreateFilterResult = await fetch("https://api.cloudflare.com/client/v4/zones/28f4cfd3041c170b9613d52b51d6b47e/filters", {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "X-Auth-Email": "lucasbradley4859@gmail.com",
+            "Authorization": "Bearer V54IDqR-eeJsy6ZhBAAcIE8s5agZVPv2-bZbe-YX"
+        },
+        body: JSON.stringify([{
+            "expression": "(ip.src eq " + jsonEventsResult.products.botd.data.ip + ")"
+        }]),
+        redirect: 'follow'
+    });
+    let jsonCreateFilterResult = await rawCreateFilterResult.json();    
+
+    // Check if the filters were successfully created
+    if (rawCreateFilterResult.status !== 200) {
+        if (jsonCreateFilterResult.errors[0].code === 10102) {
+            res.status(200).send(JSON.stringify({
+                result: 200,
+                description: 'User already blocked',
+                error: jsonCreateFilterResult
+            }));
+            return;
+        }
+        
+        res.status(500).send(JSON.stringify({
+            result: 500,
+            description: 'Internal server error',
+            error: jsonCreateFilterResult
+        }));
+        return;
+    }
+
+    // Get the id of the newly created filters
+    let rawCreateRuleResult = await fetch("https://api.cloudflare.com/client/v4/zones/28f4cfd3041c170b9613d52b51d6b47e/firewall/rules", {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "X-Auth-Email": "lucasbradley4859@gmail.com",
+            "Authorization": "Bearer V54IDqR-eeJsy6ZhBAAcIE8s5agZVPv2-bZbe-YX"
+        },
+        body: JSON.stringify([{
+            "filter": {
+                "id": jsonCreateFilterResult.result[0].id
+            },
+            "action": "block",
+            "description": "block-" + req.body.visitorId + "-ip"
+        }]),
+        redirect: 'follow'
+    })
+    let jsonCreateRuleResult = await rawCreateRuleResult.json();
+
+    // Check if the rule was successfully created
+    if (rawCreateRuleResult.status !== 200) {                
+        let rawFilterDeleteResult = await fetch("https://api.cloudflare.com/client/v4/zones/28f4cfd3041c170b9613d52b51d6b47e/filters/" + jsonCreateFilterResult.result[0].id, {
+            method: 'DELETE',
+            headers: {
+              "X-Auth-Email": "lucasbradley4859@gmail.com",
+              "Content-Type": "application/json",
+              "Authorization": "Bearer V54IDqR-eeJsy6ZhBAAcIE8s5agZVPv2-bZbe-YX"
+            },
+            redirect: 'follow'
+        });
+        let jsonFilterDeleteResult = await rawFilterDeleteResult.json();
+
+        res.status(500).send(JSON.stringify({
+            result: 500,
+            description: 'Internal server error',
+            error: rawCreateRuleResult,
+            operation: jsonFilterDeleteResult
+        }));
+        return;
+    }
+
+    res.status(200).send(JSON.stringify({
+        result: 200,
+        description: 'Bot blocked'
+    }));
+
+    console.log('**** CREATE RESULT ****')
+    console.log(jsonCreateRuleResult);
+
+    await new Promise(resolve => setTimeout(resolve, 60000));
+
+    let rawDeleteResult = await fetch("https://api.cloudflare.com/client/v4/zones/28f4cfd3041c170b9613d52b51d6b47e/firewall/rules/" + jsonCreateRuleResult.result[0].id + "?delete_filter_if_unused=true", {
+        method: 'DELETE',
+        headers: {
+            "Content-Type": "application/json",
+            "X-Auth-Email": "lucasbradley4859@gmail.com",
+            "Authorization": "Bearer V54IDqR-eeJsy6ZhBAAcIE8s5agZVPv2-bZbe-YX"
+        },
+        redirect: 'follow'
+    });
+    let jsonDeleteResult = await rawDeleteResult.json();
+
+    console.log('**** DELETE RESULT ****')
+    console.log(jsonDeleteResult);
+
+});
+
+//const client = redis.createClient();
+const port = process.env.PORT || 3000;
+app.listen(port, (err) => {
+    if (err) console.log(err);
+    console.log(`Listening on port ${port}...`)
+});
